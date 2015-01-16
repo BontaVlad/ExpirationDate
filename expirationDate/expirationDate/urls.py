dd39e1b5552ff9295d4546f935a46cdd6f84ba44
Bonta Vlad <bonta.vlad@gmail.com>
2015-01-16 08:34:27 +0200

    Better front end non-admin pages

--- expirationDate/expirationDate/urls.py
+++ expirationDate/expirationDate/urls.py
@@ -3,11 +3,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.contrib import admin
 
-from expirationDate.views import HomePage
+from expirationDate.views import HomePage, HelpPage, LicensePage
 
 urlpatterns = patterns(
     '',
     url(r'^$', HomePage.as_view(), name='home'),
+    url(r'^help$', HelpPage.as_view(), name='help'),
+    url(r'^license$', LicensePage.as_view(), name='license'),
     (r'^grappelli/', include('grappelli.urls')),  # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
 )
--- expirationDate/expirationDate/views.py
+++ expirationDate/expirationDate/views.py
@@ -3,3 +3,11 @@ from django.views.generic.base import TemplateView
 
 class HomePage(TemplateView):
     template_name = "homepage.html"
+
+
+class HelpPage(TemplateView):
+    template_name = "helppage.html"
+
+
+class LicensePage(TemplateView):
+    template_name = "licensepage.html"
